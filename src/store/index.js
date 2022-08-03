c347dd4004901e2201bd3effa3ef9a792363de38
lubazzana <lucreciabazzana@gmail.com>
2022-08-03 20:56:25 -0300

    creación de realtime database en firebase + incorporación de redux-thunk

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,12 @@
-import { combineReducers, createStore } from "redux";
+import { applyMiddleware, combineReducers, createStore } from "redux";
 
 import categoryReducer from "./reducers/category.reducer";
 import productsReducer from "./reducers/products.reducer";
+import thunk from "redux-thunk";
 
 const RootReducer = combineReducers({
     category: categoryReducer,
     product: productsReducer,
 })
 
-export default createStore(RootReducer);
+export default createStore(RootReducer, applyMiddleware(thunk));
